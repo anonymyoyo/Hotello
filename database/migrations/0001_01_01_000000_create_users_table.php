79f1e392952d0f92f3aa34f8efb4d6d530e95495
anonymyoyo <paulemmanuel@gmail.com>
2024-06-10 19:13:43 -0700

    gestion utilisateur debut

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('contact')->nullable();
+            $table->string('addresse')->nullable();
+            $table->string('ville')->nullable();
+            $table->string('pays')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
