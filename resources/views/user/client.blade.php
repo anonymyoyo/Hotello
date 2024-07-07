a545d4c8b63b72f43ad16fbed42b6459739fe4c5
anonymyoyo <paulemmanuel@gmail.com>
2024-07-07 08:41:54 -0700

    home page

---
+++ resources/views/user/client.blade.php
@@ -0,0 +1,30 @@
+<section class="py-0 py-md-5">
+	<div class="container">
+		<div class="row g-4 g-lg-7 justify-content-center align-items-center">
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/01.svg" class="grayscale" alt="">
+			</div>
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/02.svg" class="grayscale" alt="">
+			</div>
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/03.svg" class="grayscale" alt="">
+			</div>
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/04.svg" class="grayscale" alt="">
+			</div>
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/05.svg" class="grayscale" alt="">
+			</div>
+			<!-- Image -->
+			<div class="col-5 col-sm-3 col-xl-2">
+				<img src="assets/images/client/06.svg" class="grayscale" alt="">
+			</div>
+		</div>
+	</div>
+</section>
