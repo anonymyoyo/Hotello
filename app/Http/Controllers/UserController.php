e00459910e40020ca2a4bbd75eb4d957a0fb5745
anonymyoyo <paulemmanuel@gmail.com>
2024-08-25 08:19:15 -0700

    user profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,6 @@ class UserController extends Controller
 {
     //
     public function user_dashboard(){
-        return view('user.profile');
+        return view('user.user.profile');
     }
 }
---
+++ resources/views/user/user/script.blade.php
@@ -0,0 +1,11 @@
+
+<!-- Bootstrap JS -->
+<script src="{{ asset('assets/images/logo-light.svg') }}"></script>
+
+<!-- Vendors -->
+<script src="{{ asset('assets/images/logo-light.svg') }}"></script>
+<script src="{{ asset('assets/images/logo-light.svg') }}"></script>
+<script src="{{ asset('assets/images/logo-light.svg') }}"></script>
+
+<!-- ThemeFunctions -->
+<script src="assets/js/functions.js"></script>
